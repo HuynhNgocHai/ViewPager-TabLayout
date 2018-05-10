a3b61db66cff3be81faea2a27048245a43677168
Huỳnh Ngọc Hải <moyes.babes@gmail.com>
2018-05-10 16:42:20 +0700

    ViewPager&TabLayout

---
+++ app/src/main/java/com/example/pc/tablayoutviewpager/IOSFragment.java
@@ -0,0 +1,16 @@
+package com.example.pc.tablayoutviewpager;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IOSFragment extends Fragment {
+     public IOSFragment(){
+     }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ios,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/pc/tablayoutviewpager/PHPFragment.java
@@ -0,0 +1,17 @@
+package com.example.pc.tablayoutviewpager;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PHPFragment extends Fragment {
+
+    public PHPFragment(){
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_php,container,false);
+    }
+}
